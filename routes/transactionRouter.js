e25235fba145f21cf9572dc181e4d81c6f0e0d6c
Ahmeddavids <ahmeddavids6@gmail.com>
2024-02-10 03:13:53 +0100

    Added routes for withdrawal

---
+++ routes/transactionRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { payment, allPayments, confirmPayment, declinePayment, allUserPayment } = require('../controllers/depositController');
+const { authenticate, isAdmin } = require('../utils/authentication');
+const { withdraw, confirmWithdraw, declineWithdraw } = require('../controllers/withdrawController');
+const upload = require('../utils/multer')
+
+const router = express.Router();
+
+router.post('/deposit', upload.single('proofOfPayment'),authenticate, payment);
+
+router.get('/all-deposits', authenticate, isAdmin, allPayments);
+
+router.get('/all-user-deposits', authenticate, allUserPayment )
+
+router.post('/confirm-deposit/:id', authenticate, isAdmin, confirmPayment);
+
+router.get('/decline-deposit/:id', authenticate, isAdmin, declinePayment);
+
+router.post('/withdraw-confirm/:id', authenticate, isAdmin, confirmWithdraw);
+
+router.post('/withdraw-decline/:id', authenticate, isAdmin, declineWithdraw);
+
+// router.get('/transactions', authenticate, allUserTransaction);
+
+router.post('/withdraw', authenticate, withdraw);
+
+module.exports = router

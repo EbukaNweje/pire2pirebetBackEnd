8a7a088166c43028aecf4cd964b15cb4381200dc
Ahmeddavids <ahmeddavids6@gmail.com>
2024-03-16 15:29:41 +0100

    Games model set up

---
+++ models/gameModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema({
+    game: {
+        type: String,
+        require: true
+    },
+    pick: {
+        type: String,
+        require: true
+    },
+    stake: {
+        type: Number,
+        require: true
+    },
+    user: {
+        id: {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: 'User',
+            require: true
+        },
+        name: {
+            type: String,
+            require: true
+        }
+    },
+    offers: [{
+        offerStake: {
+            type: Number
+        },
+        offerType: {
+            type: Number
+        },
+        offerStatus: {
+            type: String,
+            enum: ['Pending', 'Confirmed']
+        },
+        offerBy: {
+            id: {
+                type: mongoose.SchemaTypes.ObjectId,
+                ref: 'User',
+            },
+            name: {
+                type: String,
+            }
+        },
+    }]
+}, { timestamps: true });
+
+const gameModel = mongoose.model('Game', gameSchema);
+
+module.exports = gameModel;

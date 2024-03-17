9693da1311961ed6325b0b39a702eba7820aaf59
Ahmeddavids <ahmeddavids6@gmail.com>
2024-03-17 17:42:52 +0100

    Added make offer route

--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { bookGame } = require('../controllers/gameController');
+const { bookGame, makeOffer } = require('../controllers/gameController');
 const { authenticate } = require('../utils/authentication');
 const router = express.Router();
 
 router.post('/stake', authenticate, bookGame);
+router.post('/offer/:id', authenticate, makeOffer);
 
 module.exports = router;
